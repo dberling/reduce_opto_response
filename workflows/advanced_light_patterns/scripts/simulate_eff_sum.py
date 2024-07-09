Fix bug in intensity calculation

workflows/advanced_light_patterns/scripts/simulate_eff_sum.py
@@ -26,7 +26,7 @@ soma.insert('g_chanrhod')
 
 thresh_int = np.load(str(snakemake.input[0]))
 
-rel_intensities = np.round(np.array(snakemake.params.rel_intensity) * thresh_int, 10)
+rel_intensities = np.array(snakemake.params.rel_intensity)
 intensities = np.round(rel_intensities * thresh_int, 10)
 
 # Define recording variables