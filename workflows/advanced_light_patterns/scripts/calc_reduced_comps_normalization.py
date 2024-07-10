567b8e5c49404ef5e7229010179c0bd129fac391
dberling <david.berling@rwth-aachen.de>
2024-07-10 19:41:36 +0200

    Cast column datatype of rec_cond_locs to str

--- workflows/advanced_light_patterns/scripts/calc_reduced_comps_normalization.py
+++ workflows/advanced_light_patterns/scripts/calc_reduced_comps_normalization.py
@@ -4,6 +4,7 @@ import numpy as np
 data = pd.concat([pd.read_csv(fname, index_col=0) for fname in list(snakemake.input)])
 
 summed_cond = data.groupby(['rec_cond_locs','norm_power_mW_of_MultiStimulator']).rescaled_cond_nS.sum().reset_index()
+summed_cond['rec_cond_locs'] = summed_cond['rec_cond_locs'].astype(str)
 pivot = summed_cond.pivot(index='norm_power_mW_of_MultiStimulator', columns='rec_cond_locs', values='rescaled_cond_nS')
 for col in pivot.columns:
     # assume rec_cond_locs=='9999' means all compartments
