471564da85760c723a959a65635673a389764d18
dberling <david.berling@rwth-aachen.de>
2024-11-29 17:31:19 +0100

    Fix typo

--- workflows/new_clustering/scripts/compile_adex_model.py
+++ workflows/new_clustering/scripts/compile_adex_model.py
@@ -4,4 +4,4 @@ import pickle
 module_name, neuron_model_name = NESTCodeGeneratorUtils.generate_code_for(str(snakemake.input))
 
 with open(str(snakemake.output), 'wb') as file:
-    pickle.dump((var1, var2), file)
+    pickle.dump((module_name, neuron_model_name), file)
