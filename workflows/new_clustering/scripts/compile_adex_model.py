8333694fe2116d13e41e8a652e22e4526c0bdba9
dberling <david.berling@rwth-aachen.de>
2024-11-30 14:18:05 +0100

    Workflow update

--- workflows/new_clustering/scripts/compile_adex_model.py
+++
@@ -1,7 +0,0 @@
-from pynestml.codegeneration.nest_code_generator_utils import NESTCodeGeneratorUtils
-import pickle
-
-module_name, neuron_model_name = NESTCodeGeneratorUtils.generate_code_for(str(snakemake.input))
-
-with open(str(snakemake.output), 'wb') as file:
-    pickle.dump((module_name, neuron_model_name), file)
