9f8501c40c2e172d6e539922c5ced3e62a4aa358
dberling <david.berling@rwth-aachen.de>
2025-04-02 16:41:49 +0200

    Deleting old workflows

--- workflows/rescale_factors/scripts/merge_data.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-data = pd.concat([pd.read_csv(fname, index_col=0) for fname in list(snakemake.input)])
-
-data.to_csv(str(snakemake.output))
--- workflows/scripts/plot_act_vs_pass.py
+++
@@ -1,7 +0,0 @@
-from neurostim.reduced_model_activation_plots import plot_active_vs_passive_cell_activation
-
-fig, ax = plot_active_vs_passive_cell_activation(
-        cell=snakemake.wildcards.cell_id, 
-        path='spatial_data'
-)
-fig.savefig(str(snakemake.output), dpi=snakemake.params.dpi)
