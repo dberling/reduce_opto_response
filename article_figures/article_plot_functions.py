4d65a3409673bc8b8e72472c71754c2720067006
dberling <david.berling@rwth-aachen.de>
2025-05-20 14:24:45 +0200

    Add diff plot to figure 3

--- article_figures/article_plot_functions.py
+++ article_figures/article_plot_functions.py
@@ -37,12 +37,12 @@ def map_patt_id_2_xy(df, xy_max, dxy, rotate):
             df['y'] = df['y'] * (-1) #+ dxy
     return df
     
-def subplot(df, ax, APCmax, cmap, shift_x=0, shift_y=0):
+def subplot(df, ax, APCmax, cmap, shift_x=0, shift_y=0, APCmin=0):
     pivot = df.pivot(index='x', columns='y', values='APC')
     xx, yy = np.meshgrid(pivot.index, pivot.columns, indexing='ij')
     xx += shift_x
     yy += shift_y
-    mappable = ax.pcolormesh(xx, yy, pivot.values, shading='nearest', cmap=cmap, vmin=0, vmax=APCmax)
+    mappable = ax.pcolormesh(xx, yy, pivot.values, shading='nearest', cmap=cmap, vmin=APCmin, vmax=APCmax)
     return ax, mappable
 
 def spatial_activation_plot(ax, df, APCmax, cell_topview_simtree, morph_lw, shift_x, sb_draw=1, sb_width=2, label_fiber=False):
@@ -89,6 +89,56 @@ def spatial_activation_plot(ax, df, APCmax, cell_topview_simtree, morph_lw, shif
         sb_draw=sb_draw
     )
     return ax, mappable
+
+def spatial_activation_diff_plot(ax, df_full, df_reduced, APCmin, APCmax, cell_topview_simtree, morph_lw, sb_draw=1, sb_width=2, label_fiber=False):
+    df = df_reduced[['x','y','APC']].merge(df_full[['x','y','APC']], on=['x','y'])
+    df['APC'] = (df.APC_x - df.APC_y) / df.APC_y
+    
+    print("APCmax: ",df.APC.max())
+    print("APCmin: ",df.APC.min())
+    # prepare cmap:
+    cmap = 'PuOr'
+    # plot activation
+    ax, mappable = subplot(df, ax, APCmin=APCmin, APCmax=APCmax, cmap=cmap)
+    
+    ax.spines["top"].set_visible(False)
+    ax.spines["bottom"].set_visible(False)
+    ax.spines["right"].set_visible(False)
+    ax.spines["left"].set_visible(False)
+    ax.set_xticks([])
+    ax.set_yticks([])
+    ax.axis('equal')
+    ax.set_ylim(-126,126)
+    
+    # Create unfilled circle for fiber
+    circle_center = (-100, -100)  # Circle center in data coordinates
+    circle_radius = 25
+    arrow_length = 50
+    facecolor = LinearSegmentedColormap.from_list('custom_blues', ['white', 'blue'], N=256)(0.8)
+    circle = patches.Circle(circle_center, circle_radius, edgecolor='black', facecolor=facecolor, linewidth=2)
+    ax.add_patch(circle)
+    # and arrows with movement directions
+    # Arrow pointing UP
+    ax.arrow(circle_center[0], circle_center[1] + circle_radius, 0, arrow_length, 
+             head_width=5, head_length=10, fc='black', ec='black')
+    # Arrow pointing RIGHT
+    ax.arrow(circle_center[0] + circle_radius, circle_center[1], arrow_length, 0, 
+             head_width=5, head_length=10, fc='black', ec='black')
+    # label
+    if label_fiber:
+        ax.text(circle_center[0], circle_center[1] - circle_radius, 'optical\nfiber',
+                ha='left', va='top', fontsize=8)
+    
+    # Plot top view of cell
+    cell_topview_simtree.plot2DMorphology(
+        ax=ax,
+        use_radius=True,
+        plotargs={'c': 'k', 'lw': morph_lw, 'alpha':1},
+        sb_width=sb_width,
+        sb_draw=sb_draw
+    )
+    return ax, mappable
+
     
 def plot_morphology_with_light(ax, simtree, marklocs, locargs, morph_lw, light_prof_xx_zz_I, inset=False, inset_data=None, sb_width=5.0, sb_draw=True, cax=None, cbar_kws=dict()):
     custom_cmap = LinearSegmentedColormap.from_list('custom_blues', ['white', 'blue'], N=256)
